8f837a45c79131cd471e5718c1ab9015bf96aae1
imashkan <ashkanpj.81@gmail.com>
2016-02-09 08:09:56 +0330

    a

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,7 @@ function create_config( )
     "all",
     "leave_ban",
     "admin",
-    "filemanager"
+    "feedback"
     },
     sudo_users = {81444630},--Sudo users
     disabled_channels = {},
