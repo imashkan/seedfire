89ac991edf2a1ba3deba39b242f6a8b99c88ca1e
imashkan <ashkanpj.81@gmail.com>
2016-02-21 13:04:50 +0330

    updated haul bot

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -6,7 +6,7 @@ local bot_id = our_id -- your bot id
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'متاسفیم این گروه متعلق به تله فایر نیست و فرد ادد کننده از طرف بات بلاک میشود تا نتواند در گروه دیگری ربات را اد کند', ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, 'متاسفیم این گروه متعلق به تله هیول نیست و فرد ادد کننده از طرف بات بلاک میشود تا نتواند در گروه دیگری ربات را اد کند', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
