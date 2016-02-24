32ef4c8aac4a1dfb7026ebd38c7192667452b82a
imashkan <ashkanpj.81@gmail.com>
2016-02-24 15:01:23 +0330

    updated haul bot

--- plugins/info.lua
+++ plugins/info.lua
@@ -38,7 +38,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
 	local hash = 'rank:'..extra.chat2..':variables'
 	local value = redis:hget(hash, result.id)
     if not value then
-	 if result.id == tonumber(Arian) then
+	 if result.id == tonumber (ashi) then
 	   text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
 	  elseif is_admin2(result.id) then
 	   text = text..'مقام : ادمین ربات (Admin) \n\n'
@@ -96,7 +96,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@sparta_antispam'
+  text = text..'@teleHAUL'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
@@ -135,7 +135,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@sparta_antispam'
+  text = text..'@teleHAUL'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -208,7 +208,7 @@ local function run(msg, matches)
 	 text = text..'نام گروه : '..msg.to.title..'\n'
      text = text..'ایدی گروه : '..msg.to.id
     end
-	text = text..'\n\n@sparta_antispam'
+	text = text..'\n\n@teleHAUL'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
@@ -238,8 +238,8 @@ return {
   patterns = {
 	"^([Ii][Nn][Ff][Oo])$",
 	"^([Ii][Nn][Ff][Oo]) (.*)$",
-	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
-	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+	"^([Ss][Ee][Tt][Mm][Aa][Gh][Aa][Mm]) (%d+) (.*)$",
+	"^([Ss][Ee][Tt][Mm][Aa][Gh][Aa][Mm]) (.*)$",
   },
   run = run
 }
