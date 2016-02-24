371830f0f70b64db208ea3fbfdd7cd6a857cfdb7
imashkan <ashkanpj.81@gmail.com>
2016-02-24 22:08:24 +0330

    Update and rename anti_tag to anti_tag.lua

--- plugins/anti_tag.lua
+++ plugins/anti_tag.lua
@@ -1,7 +1,7 @@
 local function run(msg)
     if msg.to.type == 'chat' and not is_momod(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'فرستادن اموجی ممنوع است'
+        return 'فرستادن تگ ممنوع است'
     end
 end
 
