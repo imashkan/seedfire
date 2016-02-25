0a869178cd6c0837b3f11bd0ddbeb5b3f46536f7
imashkan <ashkanpj.81@gmail.com>
2016-02-25 14:55:08 +0330

    updated haul bot

--- plugins/anti_fosh.lua
+++ plugins/anti_fosh.lua
@@ -1,12 +1,24 @@
 local function run(msg)
-    if msg.to.type == 'chat' and not is_momod(msg) then
-        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'استفاده از کلمات بد ممنوع است'
-    end
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['lock_fosh'] == 'yes' then
+
+
+if not is_momod(msg) then
+
+
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgtag = 'فحش دادن در این گروه غیر مجاز است بنابر این اخراج میشوید '
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
+
+      end
+   end
 end
 
-return {
-    patterns = {
+return ={
+patterns = {
     "[Kk][Ii][Rr]",
     "[Kk][Oo][Ss]",
     "[Nn][Aa][Nn][Aa][Tt]",
@@ -17,7 +29,8 @@ return {
     "کون",
     "ننت",
     "ننه",
-    "مادر"
+    "مادر",
+    "کس"
     }, 
 run = run
 }
